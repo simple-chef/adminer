87cdfb8056f0e6a9c705dd4c17a24905a3a2a82b
Mojzis Stupka <stupka@cngroup.dk>
2015-07-14 14:52:20 +0200

    added attributes

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+default['adminer']['dir_to_install'] = '/var/www/db'
+
+default['adminer']['version_to_install'] = 'mysql_english'
+
+default['adminer']['versions_available'] = {
+  :mysql_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.1-mysql-en.php',
+  :general_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.1-en.php'
+}
+
