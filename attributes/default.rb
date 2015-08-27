b56a659c3e3c83e3a61103f2871c9e2d0ecc1e39
Mojzis Stupka <stupka@cngroup.dk>
2015-08-27 22:22:48 +0200

    improved array of files to download

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['adminer']['version_to_install'] = 'mysql_english'
 
 default['adminer']['versions_available'] = {
   :mysql_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql-en.php',
-  :mysql_intl => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql-en.php',
+  :mysql_intl => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql.php',
   :general_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-en.php'
 }
 
