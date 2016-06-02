258a98988a96da6e59a67497af6898aad2385fb4
Mojzis Stupka <stupka@cngroup.dk>
2016-06-02 13:00:53 +0200

    changed download link, bumped adminer version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ default['adminer']['dir_to_install'] = '/var/www/db'
 default['adminer']['version_to_install'] = 'mysql_english'
 
 default['adminer']['versions_available'] = {
-  :mysql_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql-en.php',
-  :mysql_intl => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-mysql.php',
-  :general_english => 'http://downloads.sourceforge.net/adminer/adminer-4.2.2-en.php'
+  :mysql_english => 'https://www.adminer.org/static/download/4.2.5/adminer-4.2.5-mysql-en.php',
+  :mysql_intl => 'https://www.adminer.org/static/download/4.2.5/adminer-4.2.5-mysql.php',
+  :general_english => 'https://www.adminer.org/static/download/4.2.5/adminer-4.2.5-en.php'
 }
 
